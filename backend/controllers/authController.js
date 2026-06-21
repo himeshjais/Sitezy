daf5fa98b963a7966d62409f47b012f986cdd70c
himeshjais <himeshjaiswal.hj@gmail.com>
2026-06-21 06:12:36 +0530

    Change SameSite cookie attribute from 'strict' to 'none'

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,7 +23,7 @@ export const googleAuth = async(req, res)=> {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
